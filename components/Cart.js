add4df8548c13ffbfa26ff49fc5141fdf08b42d3
Wael Fadhel <wael.fadhel93@gmail.com>
2021-11-29 21:55:44 +0100

    adding cart content

--- components/Cart.js
+++ components/Cart.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image"
+import { Toast } from 'react-bootstrap';
+
 
 // image imports
 import CartImage from "../assets/cart.svg";
 
 function Cart() {
+    const [showCartContent, setShowCartContent] = useState(false);
+    const toggleShowCartContent = () => setShowCartContent(!showCartContent);
     return (
         <div className="cart">
-            <button>
-                 <Image src={CartImage} alt="cart" width={54} height={54}/>
-                 <span className="cart-counter">1</span>
+            <button onClick={toggleShowCartContent}>
+                <Image src={CartImage} alt="cart" width={54} height={54} />
+                <span className="cart-counter">1</span>
             </button>
+
+            <Toast show={showCartContent} onClose={toggleShowCartContent}>
+                <Toast.Header></Toast.Header>
+                <Toast.Body>
+                    <div className="cart-products">
+                        <div className="cart-product">
+                            <div className="cp-left">
+                                <h3>Samurai King Resting</h3>
+                                <span>$10000.00</span>
+                            </div>
+                            <div className="cp-right">
+                                <Image src={CartImage} />
+                            </div>
+                        </div>
+                    </div>
+                    <button onClick={() => console.log('clear')} className="cart-clear">Clear</button>
+                </Toast.Body>
+            </Toast>
+
         </div>
     )
 }
